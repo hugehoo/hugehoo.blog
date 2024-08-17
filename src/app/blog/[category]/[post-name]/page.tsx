0943f72175fa273d933cdcc05bb436d87c080510
hoo <tbnsok40@gmail.com>
2024-08-18 01:38:18 +0900

    fix: 불필요 코드 정리

--- src/app/blog/[category]/[post-name]/page.tsx
+++ src/app/blog/[category]/[post-name]/page.tsx
@@ -7,34 +7,11 @@ import PostMarkdown from "@/app/blog/[category]/[post-name]/postMarkdown";
 import localFont from "next/font/local";
 
 
-const naSquare = localFont({
+const nanumSquare = localFont({
   src: [
     {
-      path: '../../../../../public/fonts/NanumSquareNeo-Variable.woff',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../../../../public/fonts/NanumSquareNeoTTF-aLt.woff',
-      weight: '400',
-      style: 'normal',
-    },
-    {
       path: '../../../../../public/fonts/NanumSquareNeoTTF-bRg.woff',
-      weight: '400',
-      style: 'normal',
-    }, {
-      path: '../../../../../public/fonts/NanumSquareNeoTTF-cBd.woff',
-      weight: '400',
-      style: 'normal',
-    }, {
-      path: '../../../../../public/fonts/NanumSquareNeoTTF-dEb.woff',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../../../../public/fonts/NanumSquareNeoTTF-eHv.woff',
-      weight: '400',
+      weight: '500',
       style: 'normal',
     },
   ],
@@ -51,26 +28,51 @@ interface Props {
   }
 }
 
+interface PostData {
+  title: string;
+
+  [key: string]: any;
+}
+
+interface Post {
+  data: PostData;
+  content: string;
+}
+
+
+// 기본 Post 객체 정의
+const defaultPost: Post = {
+  data: {
+    title: 'Not Found',
+  },
+  content: 'The requested post was not found.'
+};
+
+
+const POSTS_PATH = path.join(process.cwd(), '/src/posts');
+
+const findPostByTitle = (decodedTitle: string): Post | undefined => {
+  const folderPath = path.join(POSTS_PATH, "**");
+  let post = sync(`${folderPath}/*.mdx`)
+    .map((filePath): Post => {
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const {data, content} = matter(fileContent);
+      return {data: data as PostData, content};
+    })
+    .find(post => post.data.title === decodedTitle);
+  return post || defaultPost;
+};
+
 const Post = ({params}: Props) => {
   const {category, 'post-name': title, thumbnail, content} = params
   let decodedTitle = decodeURIComponent(title);
-  const POSTS_PATH = path.join(process.cwd(), '/src/posts');
-  const folder = '**';
-  const postPaths = sync(`${POSTS_PATH}/${folder}/*.mdx`);
-  let filter = postPaths.map(post => {
-    const file = fs.readFileSync(post, 'utf8');
-    const {data, content} = matter(file);
-    return {data, content}
-  }).filter(data => data.data.title === decodedTitle);
-
-  const mdx = filter[0]
+  const mdx = findPostByTitle(decodedTitle)
   return (
     <div className={styles.textContainer}>
-      <h1>Category: {category}</h1>
-      <h2>Title: {decodedTitle}</h2>
-      {/*<div className={`${roboto.variable} ${styles.wrapper}`}>*/}
-      <div className={`${naSquare.variable} ${styles.wrapper}`}>
-        <PostMarkdown params={{decodedTitle, content: mdx.content}}/>
+      <div className={`${nanumSquare.variable} ${styles.wrapper}`}>
+        <h1>Category: {category}</h1>
+        <h2>Title: {decodedTitle}</h2>
+        <PostMarkdown params={{decodedTitle, content: mdx!!.content}}/>
       </div>
     </div>
   )
