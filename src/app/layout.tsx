85476ce1220224529e4654f98a7602258483fb69
Elric.Lim <tbnsok40@gmail.com>
2025-02-10 06:45:26 +0900

    feat: CRDT & OT (#9)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,53 @@
-'use client'
+'use client';
 
-import "./globals.css";
-import {IBM_Plex_Sans_KR, IBM_Plex_Sans} from 'next/font/google'
-import localFont from 'next/font/local'
-import Head from "next/head";
-import {Header} from "@/layouts/Header";
-import {Bottom} from "@/layouts/Bottom";
-import styles from "@/app/blog/TeamPage.module.css";
-import {useSelectedLayoutSegments} from "next/navigation";
+import './globals.css';
+import { IBM_Plex_Sans_KR, IBM_Plex_Sans } from 'next/font/google';
+import localFont from 'next/font/local';
+import Head from 'next/head';
+import { Header } from '@/layouts/Header';
+import { Bottom } from '@/layouts/Bottom';
+import styles from '@/app/blog/TeamPage.module.css';
+import { useSelectedLayoutSegments } from 'next/navigation';
 
 const ibmPlexSansKr = IBM_Plex_Sans_KR({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
   variable: '--font-ibm-plex-kr',
-})
+});
 
 const ibmPlexSans = IBM_Plex_Sans({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
   variable: '--font-ibm-plex',
-})
+});
 
-export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   let segment: string[] = useSelectedLayoutSegments();
 
   return (
-    <html lang="en" className={`${ibmPlexSansKr.variable} ${ibmPlexSans.variable}`}>
+    <html
+      lang="en"
+      className={`${ibmPlexSansKr.variable} ${ibmPlexSans.variable}`}
+    >
+      <title>Tech Blog Team Page</title>
+
       <Head>
-        <title>Tech Blog Team Page</title>
-        <meta name="description" content="Our team building a world of positive consumption"/>
-        <link rel="icon" href="/src/app/favicon.ico"/>
+        <meta
+          name="description"
+          content="Our team building a world of positive consumption"
+        />
+        <link rel="icon" href="/src/app/favicon.ico" />
       </Head>
       <body>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <Header/>
-          {children}
-          <Bottom/>
-        </main>
-      </div>
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <Header />
+            {children}
+            <Bottom />
+          </main>
+        </div>
       </body>
     </html>
   );
