59f7cdf3190b39e6de39dd635b9c991f82b9c3e7
Elric.Lim <tbnsok40@gmail.com>
2025-02-11 06:35:14 +0900

    fix: update footer margin (#10)

--- src/layouts/Bottom.tsx
+++ src/layouts/Bottom.tsx
@@ -2,12 +2,14 @@ import styles from '@/app/blog/TeamPage.module.css';
 
 export const Bottom = () => {
   return (
-    <footer className={styles.bottom}>
-      <a href="https://www.instagram.com/huge.hoo/">
-        <p className="text-gray-500 hover:text-gray-600 transition-colors duration-300">
-          @huge.hoo
-        </p>
-      </a>
+    <footer className={styles.footer}>
+      <div className={styles.bottom}>
+        <a href="https://www.instagram.com/huge.hoo/">
+          <p className="text-gray-500 hover:text-gray-600 transition-colors duration-300">
+            @huge.hoo
+          </p>
+        </a>
+      </div>
     </footer>
   );
 };
