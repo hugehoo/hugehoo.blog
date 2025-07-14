f946e7213b025b2c92795ae4134f223156d29bd4
hoo <tbnsok40@gmail.com>
2025-07-14 21:56:00 +0900

    add bottom link

--- src/layouts/Bottom.tsx
+++ src/layouts/Bottom.tsx
@@ -3,10 +3,15 @@ import styles from '@/app/blog/TeamPage.module.css';
 export const Bottom = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.bottom}>
+      <div className={`${styles.bottom} flex gap-2`}>
         <a href="https://www.instagram.com/huge.hoo/">
-          <p className="text-gray-500 hover:text-gray-600 transition-colors duration-300">
+          <p className="text-gray-500 hover:text-gray-800 transition-colors duration-300">
             @huge.hoo
+          </p>
+        </a>
+        <a href="https://blog-scrapper-ui.vercel.app/">
+          <p className="text-gray-500 hover:text-gray-600 transition-colors duration-300">
+            @blog-scrapper
           </p>
         </a>
       </div>
